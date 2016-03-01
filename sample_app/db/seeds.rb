0890ed7725373943551af0d2ee49c590ca1be031
Shaunty Rashidi <shauntyrashidi@shauntycomputer.sdcs.local>
2016-03-01 10:00:58 -0800

    Add user following

--- sample_app/db/seeds.rb
+++ sample_app/db/seeds.rb
@@ -1,3 +1,4 @@
+# Users
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
@@ -14,8 +15,17 @@ User.create!(name:  "Example User",
                password_confirmation: password)
 end
 
+# Microposts
 users = User.order(:created_at).take(6)
 50.times do
 	content = Faker::Lorem.sentence(5)
 	users.each { |user| user.microposts.create!(content: content) }
 end
+
+# Following relationships
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
